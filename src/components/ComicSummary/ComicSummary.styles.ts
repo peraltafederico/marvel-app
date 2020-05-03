07568cc133b6bd6a144f6f38eef85c7479c09348
Federico Peralta <peralta.federico.manuel@gmail.com>
2020-05-03 01:56:14 -0300

    improve responsive of comic summary component

--- src/components/ComicSummary/ComicSummary.styles.ts
+++ src/components/ComicSummary/ComicSummary.styles.ts
@@ -1,14 +1,23 @@
 import styled from 'styled-components'
+import { media } from '../../theme/media'
 
 export const Container = styled.div`
   display: flex;
   width: 100%;
   height: 750px;
   margin-bottom: 30px;
+
+  ${media.mobile} {
+    flex-direction: column;
+  }
 `
 export const CoverPage = styled.div`
   display: flex;
   flex: 1;
+
+  ${media.mobile} {
+    margin-bottom: 20px;
+  }
 `
 
 export const Image = styled.img`
@@ -19,17 +28,30 @@ export const Image = styled.img`
 export const Content = styled.div`
   margin-left: 30px;
   flex: 1;
+
+  ${media.mobile} {
+    margin: 0 0 20px 0;
+  }
 `
 
 export const Title = styled.h1`
   margin: 0 0 50px 0;
   color: ${(props): string => props.theme.content.textColor};
+
+  ${media.mobile} {
+    margin-bottom: 20px;
+    font-size: 22px;
+  }
 `
 
 export const ComicInfo = styled.div`
   margin: 50px 0;
   display: flex;
   flex-flow: column;
+
+  ${media.mobile} {
+    margin: 0;
+  }
 `
 
 export const Data = styled.span`
@@ -38,6 +60,10 @@ export const Data = styled.span`
   margin-bottom: 2px;
   font-weight: bold;
   opacity: 1;
+
+  ${media.mobile} {
+    font-size: 17px;
+  }
 `
 
 export const Description = styled.p`
@@ -48,5 +74,9 @@ export const Description = styled.p`
 
   & ul {
     padding-inline-start: 20px;
+  }
+
+  ${media.mobile} {
+    font-size: 18px;
   }
 `
--- src/containers/Layout/Layout.styles.ts
+++ src/containers/Layout/Layout.styles.ts
@@ -1,14 +1,18 @@
 import styled from 'styled-components'
+import { media } from '../../theme/media'
 
 export const ContentContainer = styled.div`
   display: flex;
   justify-content: center;
-  margin: 120px 40px 0;
+  padding: 120px 40px 0;
+
+  ${media.mobile} {
+    padding: 100px 20px 0;
+  }
 `
 
 export const Content = styled.div`
   display: flex;
   justify-content: center;
   flex-flow: row wrap;
-  width: 1200px;
 `
